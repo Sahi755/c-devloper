afe8260f0a083fe2b15ce30be6d69a2571e5d7a8
sahi755 <sahilbhosale162@gmail.com>
2025-07-21 13:36:21 +0530

    get commit

--- DSA/STL/STLS.cpp
+++ DSA/STL/STLS.cpp
@@ -1,6 +1,8 @@
 //g++ --std=c++14 STLS.cpp -o STLS && STLS
 #include<iostream>
+#include<algorithm>
 #include<vector>
+#include<map>
 using namespace std;
 class Vector{
 
@@ -11,6 +13,8 @@ class Vector{
 
                 cout<<i<<" ";
             }
+
+            cout<<endl;
         }
 
         static void pushfront(vector<int>vs){
@@ -35,6 +39,95 @@ class Vector{
              cout<<"print at back:";
              printvector(vs);
         }
+
+        static void deleteMe(vector<int>vs){
+
+              int pos;
+              cout<<"original vector:"<<endl;
+              vs.pop_back();
+              cout<<" after delete last element:";
+              printvector(vs);
+              auto it=vs.begin();
+
+              cout<<"Enter the pos:";
+              cin>>pos;
+              vs.erase(it+pos-1);
+              cout<<"delete after first element:";
+              printvector(vs);
+        }
+
+        static void Access(vector<int>vs){
+               
+               int vsize=vs.size()-1;
+               cout<<"access first eleemnt:";
+               auto it1=vs.begin(); // use also vs.front() 
+               cout<<"first:"<<*it1<<endl;
+               int mid=0+vs.size()/2;
+               cout<< "mid:"<<vs.at(mid)<<endl;
+               cout<<"accesss last element:"; // use also vs.back()
+               int it3=vs.at(vsize);
+               cout<<it3<<endl;
+            
+        }
+
+        static void check(vector<int>vs){
+
+               //vs.clear();
+
+               if(!vs.empty()){
+
+                     cout<<"size:"<<vs.size()<<endl;
+                     cout<<"capacity:"<<vs.capacity()<<endl;
+               }else{
+
+                     cout<<"vector is empty!";
+               }               
+        }
+
+        static void reversevector(vector<int>vs){
+
+         cout<<"reverse vector:";
+         vector<int>::reverse_iterator it=vs.rbegin();
+
+         int i=0;
+        for(;it!=vs.rend();++it){
+
+              *it=++i;
+        }
+            for(auto it=vs.begin(); it!=vs.end();it++){
+
+                    cout<<*it<<" ";
+            }
+        }
+
+    static void sorts(vector<int>vs){
+
+         cout<<"sort by assending order:";
+         sort(vs.begin(),vs.end());
+         printvector(vs);
+
+    }
+
+    static void  frequency(vector<int>vs){
+
+         map<int,int>counts;
+
+          for(auto i:vs){
+
+              counts[i]++;
+          }
+
+          for(auto i:counts){
+
+               cout<<i.first<<":"<<i.second<<" "<<endl;
+          }
+    }
+
+    static void maxof(vector<int>vs){
+
+         
+    }
+
 };
 int main(){
     
@@ -54,7 +147,7 @@ int main(){
                vs.push_back(k);
         
           }
-          Vector::pushBack(vs);
+          Vector::frequency(vs);
           vs.clear();
     }
 }
