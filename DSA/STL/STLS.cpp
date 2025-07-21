5b733d7af648fb0db676dbf547bc52ee15a0734a
sahi755 <sahilbhosale162@gmail.com>
2025-07-21 10:50:26 +0530

    get commit

--- DSA/STL/STLS.cpp
+++ DSA/STL/STLS.cpp
@@ -1,8 +1,60 @@
+//g++ --std=c++14 STLS.cpp -o STLS && STLS
 #include<iostream>
+#include<vector>
 using namespace std;
 class Vector{
+
+     public:
+        static void printvector(vector<int>vs){
+
+            for(auto i:vs){
+
+                cout<<i<<" ";
+            }
+        }
+
+        static void pushfront(vector<int>vs){
+
+              int num;
+              int pos;
+              cout<<"Enter the num:";
+              cin>>num;
+              auto it=vs.begin();
+              vs.insert(it,num);
+              printvector(vs);
+
+        }
+
+        static void pushBack(vector<int>vs){
+
+             int num;
+             cout<<"Enter the num:";
+             cin>>num;
+
+             vs.push_back(num);
+             cout<<"print at back:";
+             printvector(vs);
+        }
 };
 int main(){
     
-      
+    int T;
+
+    cin>>T;
+
+    while(T--){
+
+         vector<int>vs;
+          int n;
+          cin>>n;
+          for(int i=0;i<n;i++){
+
+                int k;
+               cin>>k;
+               vs.push_back(k);
+        
+          }
+          Vector::pushBack(vs);
+          vs.clear();
+    }
 }
