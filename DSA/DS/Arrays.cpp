7f0d0af63a4de8d1f6462fb61b5f5f828727475d
sahi755 <sahilbhosale162@gmail.com>
2025-07-30 12:27:15 +0530

    get commit

--- DSA/DS/Arrays.cpp
+++ DSA/DS/Arrays.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 class Arrays{
 
@@ -18,19 +20,107 @@ class Arrays{
             cout<<"Max:"<<maxi<<endl;
        }
 
-       static void inputArr(){
+    static void secondmin(int arr[],int size){
 
+         sort(arr,arr+size);
+
+         bool allsame=false;
+
+         int secondmin=0;
+         int left=0;
+
+         int right=size-1;
+
+         while(left<=right){
+
+                if(arr[left]==arr[left+1]){
+
+                       allsame=true;
+                      
+                }
+
+                left++;
+         }
+
+         if(allsame==false){
+
+               cout<<arr[1];
+         }else{
+
+               cout<<"No second min all are same!";
+         }
+    }
+
+     static void elementSorted(int arr[],int size){
+
+         bool isSorted=true;
+
+         for(int i=0;i<size;i++){
+
+              if(arr[i]>=arr[i+1]){
+
+                   isSorted=false;
+              }
+         }
+
+         if(isSorted==true){
+            cout<<"array is sorted!";
+
+         }else{
+              cout<<"array is not sorted!";
+         }
+     }
+
+      static void reverseArray(int arr[],int n){
+
+            int start=0;
+            int end=n-1;
+            while(start<end){
+                   int temp=arr[start];
+                   arr[start]=arr[end];
+                   arr[end]=temp; 
+                   start++;
+                   end--;
+            }
+            cout<<"reverse arr:" ;
+
+            for(int i=0;i<n;i++){
+
+                 cout<<arr[i]<<" ";
+            }
+      }
+
+      static void rightRotate(int arr[],int n){
+
+               int last=arr[n-1];
+               for(int i=n-1;i>0;i--){
+
+                    arr[i]=arr[i-3];
+               }
+
+              arr[0]=last;
+              for(int i=0;i<n;i++){
+
+                  cout<<arr[i]<<" ";
+              }
+      }
+
+       static void inputArr(){
           int size;
           int arr[10];
           int T;
           cin>>T;
-          for(int i=0;i<size;i++){
-
-
+          while(T--){
+               int pos;
+               cin>>size;
+               for(int i=0;i<size;i++){
+                  cin>>arr[i];
+               }
+                  rightRotate(arr,size);
           }
        }
 };
 int main(){
 
-
+   Arrays::inputArr();
 }
--- codeforces/rating800to1000.cpp
+++ codeforces/rating800to1000.cpp
@@ -668,12 +668,13 @@ class Arrayprob{
        }
 
        static void rightShift(int arr[],int n){
+
                int last=arr[n-1];
                for(int i=n-1;i>0;i--){
 
                     arr[i]=arr[i-1];
                }
-
+               
               arr[0]=last;
               for(int i=0;i<n;i++){
               
