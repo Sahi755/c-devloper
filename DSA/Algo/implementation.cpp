a726e99159a52f46d1bc738c76c9da58a3c59075
sahi755 <sahilbhosale162@gmail.com>
2025-09-17 14:30:33 +0530

    get commit

--- DSA/Algo/implementation.cpp
+++ DSA/Algo/implementation.cpp
@@ -2,6 +2,7 @@
 #include<algorithm>
 #include<vector>
 #include<vector>
+#include<cmath>
 #include<set>
 #include<string>
 using namespace std;
@@ -386,16 +387,191 @@ class Group3{
 			cout<<"min:"<<*maxi<<endl;
 	   }
 
-	    static void inputloop(){
+	   static void prob16(int num){
 
+		     for(int i=0;i<=num;i++){
+
+				   for(int j=0;j<=i;j++){
+
+					    cout<<"j";
+				   }
+
+				  cout<<endl;
+			 }
+
+	   }
+
+	    static void prob17(int num){
+
+		     for(int i=0;i<=num;i++){
+
+				   for(int j=i;j<=num;j++){
+					    cout<<"j";
+				   }
+				  cout<<endl;
+			 }
+	   }
+	
+	   static void prob18(int n){
+
+		     int num=0;
+		     for(int i=1;i<=n;i++){
+				   num+=pow(i,2);
+			 }
+			cout<<num<<endl;
+	   }
+
+	    static void inputloop(){
 			  int n;
 			  cin>>n;
-			  prob15(n);
+			  prob18(n);
 		  }
 
 };
+class Group4{
+
+	  public:
+		 static signed int BinarySearch(int arr[],int size,int target){
+
+			     int low=0;
+				 int high=size-1;
+
+				 while(low<=high){
+					  int mid=low+(high-low)/2;
+					  if(arr[mid]==target){
+						   return mid;
+					  }else if(arr[mid]>target){
+						      high=mid-1;
+					  }else{
+						      low=mid+1;
+					  }
+				 }
+			  return -1;
+		 }
+
+		 static int  firstAcuurance(int arr[],int size,int target){
+
+
+			     int low=0;
+				 int high=size-1;
+				 int result=-1;
+
+				 while(low<=high){
+					  int mid=low+(high-low)/2;
+					  if(arr[mid]==target){
+						  result=mid;
+						  high=mid-1;
+					  }else if(arr[mid]<target){
+						     low=mid+1;
+					  }else{
+						     high=mid-1;
+					  }
+				 } 
+			  return result;
+		 }
+
+		static int lastAcuurance(int arr[],int size,int target){
+
+			  int low=0;
+			  int high=size-1;
+			  int result=-1;
+
+			  while(low<=high){
+				    int mid=low+(high-low)/2;
+					if(arr[mid]==target){
+						  result=mid;
+						  low=mid+1;
+					}else if(arr[mid]<target){
+                          low=mid+1;
+					}else{
+						  high=mid-1;
+					}
+			  }
+			 return result;
+		}
+
+		static int countAccurance(int arr[],int size,int target){
+
+			 
+			    int first=firstAcuurance(arr,size,target);
+				if(first==-1) return 0;
+				int last=lastAcuurance(arr,size,target);
+
+				return last-first+1;
+		}
+
+		static int lowerBound(int arr[],int size,int target){
+
+		       int low=0;
+			   int high=size-1;
+			   int smallest=-1;
+
+			   while(low<=high){
+				     int mid=low+(high-low)/2;
+
+					if(arr[mid]>=target){
+						  smallest=mid;
+						  high=mid-1;
+					}else{
+						  low=mid+1;
+					}	
+			   }
+			 return smallest;
+		}
+
+	static int upperBound(int arr[],int size,int target){
+
+			  int low=0;
+			  int high=size-1;
+			  int largest=-1;
+
+			 while(low<=high){
+				    int mid=low+(high-low)/2;
+
+					if(arr[mid]<=target){
+						  largest=mid;
+						  low=mid+1;
+					}else{
+						 high=mid-1;
+					}
+			   }
+			  return  largest;
+		}
+
+	  public:
+		 static void inputArr(){
+
+			   int arr[10];
+			   int size;
+			   int target;
+			   
+
+			   cout<<"size:";
+			   cin>>size;
+
+			   cout<<"Enter the element:";
+			   for(int i=0;i<size;i++){
+				    cin>>arr[i];
+			   }
+
+			   cout<<"Enter the target:";
+			   cin>>target;
+
+                int upperbound=upperBound(arr,size,target);
+
+			   if(upperbound!=-1){
+				   cout<<"found:"<<upperbound<<endl;
+			   }else{
+				   cout<<"not found:"<<upperbound<<endl;
+			   }
+		 }
+};
+class Group5{
+
+	  
+};
 int main(){
 
-	 Group3::inputloop();
+	Group4::inputArr();
 
 }
